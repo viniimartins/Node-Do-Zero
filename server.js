763159ed491aede2c0362b0969237439dc99a2a3
viniimartinsz <vinimribeiro2004@gmail.com>
2023-09-11 11:16:39 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+import { fastify } from "fastify";
+import { DatabasePostgres } from "./database-postgres.js";
+
+const server = fastify();
+
+const database = new DatabasePostgres();
+
+// GET, POST, PUT, DELETE, PATCH,
+
+// POST http://localhost:3333/videos    => Criar video
+
+server.post("/videos", async (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  await database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
+});
+
+// GET http://localhost:3333/videos    => Listar video
+
+server.get("/videos", async (request) => {
+  const search = request.query.search;
+
+  const videos = await database.list(search);
+
+  return videos;
+});
+
+// PUT http://localhost:3333/videos/3    => Atualizar video
+
+// Route Parameter
+
+server.put("/videos/:id", async (request, reply) => {
+  const videoId = request.params.id;
+
+  const { title, description, duration } = request.body;
+
+  await database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(204).send();
+});
+
+// DELETE http://localhost:3333/videos/3    => Deletar video
+
+server.delete("/videos/:id", async (request, reply) => {
+  const videoId = request.params.id;
+
+  await database.delete(videoId);
+
+  return reply.status(204).send();
+});
+
+server.listen({
+  port: 3333,
+});
